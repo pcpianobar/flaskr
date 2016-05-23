a48a0ad03c5d9090c5abb1b2a2dc89407ebb4b0d
Kihun Jung <pcpianobar@gmail.com>
2016-05-24 08:25:39 +0900

    db 초기화

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,6 @@
 # all the imports
+from __future__ import with_statement
+from contextlib import closing
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
@@ -15,6 +17,12 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
     app.run()

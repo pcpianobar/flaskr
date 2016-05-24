af1337cce84543ac597eca98a958de4bab2a3405
Kihun Jung <pcpianobar@gmail.com>
2016-05-25 08:11:28 +0900

    튜토리얼 파일들과 기본 유닛테스트 추가.

---
+++ flaskr_test.py
@@ -0,0 +1,25 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app_context = flaskr.app.app_context()
+        self.app_context.push()
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+        self.app_context.pop()
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
